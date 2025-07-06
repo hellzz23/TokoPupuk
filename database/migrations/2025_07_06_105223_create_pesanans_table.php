e00c6e7163ce8633a6e51ef8d97d0e2e772c487a
hellzz23 <grahelsimanjuntak437@gmail.com>
2025-07-06 18:02:39 +0700

    memperbarui migrate pesanan

--- database/migrations/2025_07_06_105223_create_pesanans_table.php
+++ database/migrations/2025_07_06_105223_create_pesanans_table.php
@@ -1,8 +1,10 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use App\Models\User;
+use App\Models\Pupuk;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -13,7 +15,16 @@ return new class extends Migration
     {
         Schema::create('pesanans', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(User::class);
+            $table->foreignIdFor(Pupuk::class);
+            $table->string('nama');
+            $table->string('ponsel');
+            $table->string('alamat');
+            $table->string('jumlah');
+            $table->date('tgl_pesan');
+            $table->string('total');
             $table->timestamps();
+            $table->softDeletes('deleted_at');
         });
     }
 
