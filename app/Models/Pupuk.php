16d29ad9b671d33e3384ba2dcd430c2c8330a86d
hellzz23 <grahelsimanjuntak437@gmail.com>
2025-07-06 18:00:29 +0700

    memasukkan model pupuk (tadi lupa)

--- app/Models/Pupuk.php
+++ app/Models/Pupuk.php
@@ -3,8 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Pupuk extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'pupuks';
+    protected $primaryKey = 'id';
+    protected $fillable = ['id','user_id', 'nama', 'jenis', 'stok', 'harga', 'deskripsi', 'foto'];
+
+    public function user(): BelongsTo{
+        return $this->belongsTo(User::class);
+    }
 }
